25b603795944997333fb316b83a5256187532f62
nar2p1nk <farrisisqandar@gmail.com>
2022-03-17 02:41:20 +0800

    'createUser' created

---
+++ database.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const pg = require('pg');
+const hash = require('nanoid').nanoid(50);
+require('custom-env').env('secret')
+
+const client = new pg.Client(process.env.CONNECT_STRING2);
+
+
+client.connect();
+
+function createUser(username,password){
+    const hashedPassword = bcrypt.hashSync(password,10)
+    client.query(`INSERT INTO users(id,username,password) VALUES(1$,2$,2$)`,
+        [hash,username,hashedPassword],(err,res)=>{
+            if(err){console.error(err.stack)}
+            else{
+                console.log('user created')
+            }
+        }
+    )
+}
+
+
+
+
+
