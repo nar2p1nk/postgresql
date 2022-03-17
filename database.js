c8a42985ffdff54c4e36200c35160d19200659c1
nar2p1nk <farrisisqandar@gmail.com>
2022-03-17 22:49:13 +0800

    changed numbers of letters created by nanoid and fixed some bugs

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const pg = require('pg');
-const hash = require('nanoid').nanoid(50);
+const hash = require('nanoid').nanoid();
 require('custom-env').env('secret')
 
 const client = new pg.Client(process.env.CONNECT_STRING2);
@@ -10,17 +10,17 @@ client.connect();
 
 function createUser(username,password){
     const hashedPassword = bcrypt.hashSync(password,10)
-    client.query(`INSERT INTO users(id,username,password) VALUES(1$,2$,2$)`,
+    client.query(`INSERT INTO users(id,username,password) VALUES($1,$2,$3)`,
         [hash,username,hashedPassword],(err,res)=>{
             if(err){console.error(err.stack)}
             else{
                 console.log('user created')
             }
+            client.end()
         }
     )
 }
-
-
+//createUser('emilia', 'Fi')
 
 
 
